89a22d41864e8c5829cf30452e6d2ee6e3ff9e9d
Andy Feidje Joseph <ingandy75@gmail.com>
2019-08-14 15:11:50 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,13 @@
 class Song < ActiveRecord::Base
+  
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def genre_name= name
+    #since the song has no genre, we have to create it
+    genre ? genre.name  : nil
+  end
+  
 end
 
