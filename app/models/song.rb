f0eb24ee85b90e8bb0dcba117bdd6dfa8f253e18
Andy Feidje Joseph <ingandy75@gmail.com>
2019-08-14 15:19:52 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,4 +9,29 @@ class Song < ActiveRecord::Base
     self.genre  = Genre.find_or_create_by name: name
   end
 
+  def genre_name
+    # if the song has a genre return the genre's name else return nil
+    genre ? genre.name  : nil
+  end
+
+  def artist_name=  name
+    self.artist = Artist.find_or_create_by  name: name
+  end
+
+  def artist_name
+  artist  ? artist.name : nil
+  end
+
+  def note_contents=  contents
+    contents.each do |c|
+      self.notes << Note.find_or_create_by(content: c) unless c.empty
+    end
+  end
+
+  def note_contents
+    notes.map do |n| 
+      n.content
+    end
+  end
+
 end
